2475d859b949d5e73787b12ea8d1bde7c85224fa
Zachary Cush <zachcushonline@gmail.com>
2021-01-27 15:35:38 -0800

    Render all components

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 const PORT = 3000;
 
 app.use(express.json());
