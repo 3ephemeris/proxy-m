3b17d9ec90bf6cae60b2bff6b2b8980ff3cb83ae
Zachary Cush <zachcushonline@gmail.com>
2021-01-30 09:56:57 -0800

    Update bundle for reviews

--- server/index.js
+++ server/index.js
@@ -13,11 +13,11 @@ app.use(express.json());
 //size-carousel
 app.use('/api/items', createProxyMiddleware({ target: 'http://ec2-52-53-215-61.us-west-1.compute.amazonaws.com:3001/', changeOrigin: true }));
 //reviews
-app.use('/products', createProxyMiddleware({ target: 'http://54.187.133.21:3003', changeOrigin: true }));
+app.use('/productreviews', createProxyMiddleware({ target: 'http://54.187.133.21:3003', changeOrigin: true }));
 //similar-products
-app.use('/api/products/:productId', createProxyMiddleware({ target: 'http://localhost:3003', changeOrigin: true }));
-app.use('/api/wishlist/:productId', createProxyMiddleware({ target: 'http://localhost:3003', changeOrigin: true }));
-app.use('/api/wishlist/:productId', createProxyMiddleware({ target: 'http://localhost:3003', changeOrigin: true }));
+app.use('/api/products/:productId', createProxyMiddleware({ target: 'http://3.101.140.254:3002/', changeOrigin: true }));
+app.use('/api/wishlist/:productId', createProxyMiddleware({ target: 'http://3.101.140.254:3002/', changeOrigin: true }));
+app.use('/api/wishlist/:productId', createProxyMiddleware({ target: 'http://3.101.140.254:3002/', changeOrigin: true }));
 
 app.listen(PORT, () => {
   console.log(`Server Listening on port ${PORT}...`);
